79e66f316d620deff13eda4c9c25d23c8403a6a2
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-09-25 16:21:09 +0700

    Release v0.003

--- lib/Sah/Schema/perl/arraydata/modname_with_optional_args.pm
+++ lib/Sah/Schema/perl/arraydata/modname_with_optional_args.pm
@@ -14,7 +14,7 @@ our $schema = Sah::PSchema::perl::modname_with_optional_args->get_schema({
     ns_prefix => "ArrayData",
 });
 
-$schema->{summary} = 'Perl ArrayData::* module name without the prefix (e.g. Lingua::Word::ID::KBBI) with optional args (e.g. Foo::Bar=arg1,arg2)';
+$schema->[1]{summary} = 'Perl ArrayData::* module name without the prefix (e.g. Lingua::Word::ID::KBBI) with optional args (e.g. Foo::Bar=arg1,arg2)';
 
 1;
 # ABSTRACT:
